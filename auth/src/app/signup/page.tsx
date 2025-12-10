3ef356ee6e5cc4ee9c46b3be39716cca7a7d8c73
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-12-10 22:15:56 +0000

    feat: comple login and signup for both frontend/backend

--- auth/src/app/signup/page.tsx
+++ auth/src/app/signup/page.tsx
@@ -1,24 +1,49 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, {useEffect} from "react";
 import { useRouter } from "next/navigation";
-//import { axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignUp() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: "",
-    })
+    });
+
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+    const [loading, setLoading] = React.useState(false);
+
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0){
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    },[user]);
 
     const onSignup = async () => {
-
+        try {
+            setLoading(true);
+           const response =  await axios.post("/api/users/signup", user);
+           console.log("Signup success", response.data);
+           router.push("/login");
+        } catch (error: any) {
+            console.log("Signup failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup</h1>
+            <h1>{loading ? "Processing": "SignUp"}</h1>
             <hr />
             <label htmlFor="username">username</label>
             <input
@@ -54,7 +79,7 @@ export default function SignUp() {
             <button
                 onClick={onSignup}
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none">
-                Signup
+                {buttonDisabled ? "No Signup" : "SignUp"}
             </button>
             <Link href="/login">Already a member? Log In</Link>
         </div>
