a6dbe0a06b85bfbe0f3db806ebe0a4ba0653a746
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-12-10 19:30:38 +0000

    feat: add a login page

--- auth/src/app/login/page.tsx
+++ auth/src/app/login/page.tsx
@@ -1,7 +1,53 @@
-export default function LoginPage(){
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+//import { axios } from "axios";
+
+export default function LoginPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    })
+
+    const onLogin = async () => {
+
+    }
+
     return (
-        <div>
-            Login
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Login</h1>
+            <hr />
+
+            <label htmlFor="email">email</label>
+            <input
+                className="p-4 border border-gray-300"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email">
+            </input>
+
+            <label htmlFor="password">password</label>
+            <input
+                className="p-4 border border-gray-300"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password">
+            </input>
+            <br />
+
+            <button
+                onClick={onLogin}
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none">
+                Signup
+            </button>
+            <Link href="/signup">Don't have account yet? Signup</Link>
         </div>
+
     );
 }
